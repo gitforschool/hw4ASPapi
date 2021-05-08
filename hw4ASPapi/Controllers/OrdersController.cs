83a2a7dc47a6a157e9828f5f6c1faf335660867a
Blondeni1981 <nicolemari922@gmail.com>
2021-05-08 15:46:50 -0700

    Worst database ever

--- hw4ASPapi/Controllers/OrdersController.cs
+++ hw4ASPapi/Controllers/OrdersController.cs
@@ -12,6 +12,7 @@ using System.Net.Http;
 using System.Web.UI.WebControls;
 using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
 using RouteAttribute = System.Web.Http.RouteAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
 
 namespace hw4ASPapi.Controllers
 {
@@ -26,64 +27,54 @@ namespace hw4ASPapi.Controllers
         public IEnumerable<object> GetOrders()
         {
 
-
             var OrderQuery =
-
                                   from o in db.Orders
                                   where o.pricePaid > 13
-                                  join s in db.StoreTables on  o.storeID equals s.storeID 
+                                  join s in db.StoreTables on o.storeID equals s.storeID
                                   group new { o, s } by s.City into orderGroup
                                   let count = orderGroup.Count()
                                   orderby count descending
                                   select new
                                   {
                                       Store = orderGroup.Key,
-                                      
+
                                       CDSales = orderGroup.Count()
                                   };
-
-
             return OrderQuery.ToList();
 
-            //                    from o in db.Orders
-            //                    where o.pricePaid > 13 
-            //                    group o by o.storeID into orderGroup
-            //                    let count = orderGroup.Count()
-            //                    orderby count descending
-            //                    select new
-            //                    {
-            //                        Store = orderGroup.Key,
-            //                        CDSales = orderGroup.Count()
-            //                    };
-            //return OrderQuery.ToList();
         }
 
         //-------------------------------------------------------------------------------------------
 
         //Prepopulate Name Drop Down
 
-        [Route ("people")]
+        [Route("people")]
         [HttpGet]
-        public IEnumerable<object> GetPeople()
+        public IEnumerable<object> GetPeopleList()
         {
 
-            var PeopleQuery = from p in db.SalesPersonTables
-                              select new
-                              {
-                                  p.FirstName,
-                                  p.LastName
-                              };
-            return PeopleQuery.ToList();
+            var PeopleQuery = (from o in db.Orders
+                               join s in db.SalesPersonTables on o.salesPersonID equals s.salesPersonID
+                               //where o.salesPersonID <= 15
+                               select new
+                               {
+                                   Person = s.FirstName
+                               });
+
+
+            var person = PeopleQuery;
+            return person.Distinct().ToList();
         }
+
 
 
         //-----------------------------------------------------------------------------------------------------
 
 
         //Prepopulate City Drop Down Menu
-       [Route("city")]
+        [Route("city")]
         [HttpGet]
-        public IEnumerable<object> GetCities()
+        public IEnumerable<object> GetCitiesList()
         {
             var CityQuery = from s in db.StoreTables
                             select new
@@ -94,5 +85,77 @@ namespace hw4ASPapi.Controllers
             return CityQuery.ToList();
         }
 
+        // ---------------------------------------------------------------------------------------------------------------
+
+
+
+        //Get Data From Selected Sales Person
+        [System.Web.Http.Route("pSales")]
+        [HttpGet]
+        public IHttpActionResult PersonSales(string id)
+        {
+
+            try
+            {
+                var zero = 0;
+               
+                var personSalesQuery = from o in db.Orders
+                                       join s in db.SalesPersonTables on o.salesPersonID equals s.salesPersonID
+                                       
+                                       where s.FirstName == id 
+                                       
+                                    //   let Person = s.FirstName + " " + s.LastName
+                                       group new { o, s } by s.FirstName into SalesPersonGroup
+
+                                       select new
+                                       {
+                                           Person = SalesPersonGroup.Key,
+                                           Price = SalesPersonGroup.Sum(x => x.o.pricePaid)
+
+                                       };
+                return Ok(personSalesQuery);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+        }
+
+        //--------------------------------------------------------------
+        //Store Sales
+
+        [System.Web.Http.Route("sSales")]
+        [HttpGet]
+        public IHttpActionResult StoreSales(string id)
+        {
+
+
+            try
+            {
+                //int myId = Int32.Parse(id);
+
+                var StoreSalesQuery = from o in db.Orders
+
+                                      join s in db.StoreTables on o.storeID equals s.storeID
+                                      where s.City == id
+                                      group new { o, s } by s.City into SalesPersonGroup
+                                      let count = SalesPersonGroup.Count()
+                                      select new
+                                      {
+                                          Store = SalesPersonGroup.Key,
+                                          Price = SalesPersonGroup.Sum(x => x.o.pricePaid)
+
+                                      };
+                return Ok(StoreSalesQuery);
+            }
+
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+        }
     }
+
 }
--- hw4ASPapi/Scripts/main.js
+++ hw4ASPapi/Scripts/main.js
@@ -63,7 +63,7 @@ $(function () {
         dataType: "json",
         success: function (r) {
             $.each(r, function (key, person) {
-                $('#person').append($('<option></option>').val(person.$id).html(person.FirstName + " " + person.LastName));
+                $('#person').append($('<option></option>').val(person.$id).html(person.Person));
             });
         }
     });
@@ -86,4 +86,122 @@ $(function () {
         }
     });
 });
+
+//----------------------------------------------------------------------------------
+
+//Function to display data to user
+
+
+function GetPeopleList() {
+
+    var select = document.querySelector("#person");
+    var id = select.options[select.selectedIndex].text;
+    console.log(id);
+
+
+    $(function () {
+        $.ajax({
+            type: "GET",
+            url: 'api/orders/pSales?id=' + id,
+            data: '{}',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (person) {
+                console.log(person)
+                EmployeeSalesListSuccess(person);
+            }
+        });
+    });
+
+}
+
+function EmployeeSalesListSuccess(person) {
+    // Iterate over the collection of data
+    $.each(person, function (index, person) {
+        // Add a row to the Order Detail table
+        EmployeeSalesAddRow(person);
+        console.log(person);
+    });
+}
+
+function EmployeeSalesAddRow(person) {
+    // Check if <tbody> tag exists, add one if not
+    if ($("#SalesTable tbody").length == 0) {
+        $("#SalesTable").append("<tbody></tbody>");
+        console.log(person);
+    }
+    // Append row to <table>
+    $("#SalesTable tbody").append(
+        EmployeeSalesBuildTableRow(person));
+}
+
+function EmployeeSalesBuildTableRow(person) {
+    var ret =
+        "<tr>" +
+        "<td>" + person.Person + "</td>" +
+        "<td>" + person.Price + "</td>" +
+        "</td>"
+    "</tr>";
+    return ret;
+}
+
+
+//---------------------------------------------------------------------------
+//Displays Store Total Sales
+
+function GetStoreList() {
+
+
+    var select = document.querySelector("#citySales");
+    var id = select.options[select.selectedIndex].text;
+    console.log(id);
+
+
+    $(function () {
+        $.ajax({
+            type: "GET",
+            url: 'api/orders/sSales?id=' + id,
+            data: '{}',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (store) {
+                console.log(store)
+                StoreSalesListSuccess(store)
+            }
+        });
+    });
+
+};
+
+function StoreSalesListSuccess(store) {
+    // Iterate over the collection of data
+    $.each(store, function (index, store) {
+        console.log(store);
+        // Add a row to the Order Detail table
+        StoreSalesAddRow(store);
+        console.log(store);
+    });
+}
+
+function StoreSalesAddRow(store) {
+    // Check if <tbody> tag exists, add one if not
+    if ($("#StoresTable tbody").length == 0) {
+        $("#StoresTable").append("<tbody></tbody>");
+        console.log(store);
+    }
+    // Append row to <table>
+    $("#StoresTable tbody").append(
+        StoreBuildTableRow(store));
+}
+
+function StoreBuildTableRow(store) {
+    var ret =
+        "<tr>" +
+        "<td>" + store.Store + "</td>" +
+        "<td>" + store.Price + "</td>" +
+        "</td>"
+    "</tr>";
+    return ret;
+}
+
 
